1c025f58938e080840557b09cea10976623e90f8
Zhani Baramidze <zhani.baramidze@bolt.eu>
2020-10-31 20:08:40 +0200

    Added functionality to read from file

--- editor.go
+++ editor.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/gdamore/tcell"
+import (
+	"strings"
+
+	"github.com/gdamore/tcell"
+)
 
 // Editor is main editor structure.
 type Editor struct {
@@ -14,6 +18,30 @@ func createEditor(handler screenHandler) *Editor {
 
 func (e *Editor) startLoop() {
 	e.display.startLoop()
+}
+
+func (e *Editor) initData(data []byte) {
+	fields := strings.Fields(string(data))
+	for i, field := range fields {
+		if i == 0 {
+			e.display.getCurrentEl().data = []rune(field)
+			e.display.getCurrentEl().pos = len(field)
+		} else {
+			newItem := Line{data: []rune(field), startingCoordY: -1, height: -1, pos: 0, display: e.display}
+			e.display.data.InsertAfter(&newItem, e.display.currentElement)
+			e.display.currentElement = e.display.currentElement.Next()
+		}
+	}
+
+	// Move current to the beginning to resync
+	e.display.currentElement = e.display.data.Front()
+	e.display.resyncAll()
+
+	// Leave it at the end
+	e.display.currentElement = e.display.data.Back()
+	e.display.getCurrentEl().pos = len(e.display.getCurrentEl().data)
+	e.display.syncCoords()
+
 }
 
 func (e *Editor) pollKeyboard(resp chan bool) {
--- main.go
+++ main.go
@@ -1,19 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
 func main() {
 
-	// arg := os.Args[1]
+	if len(os.Args) != 2 {
+		fmt.Println("Error: Please pass filename as argument")
+		os.Exit(1)
+	}
 
-	// data, err := ioutil.ReadFile(arg)
-	// if err != nil {
-	// 	fmt.Printf("Opening file failed: %v", err)
-	// 	return
-	// }
-	//
-	//fmt.Printf("%v", data)
+	arg := os.Args[1]
+
+	data, err := ioutil.ReadFile(arg)
+	if err != nil {
+		fmt.Printf("Error: Opening file failed: %v", err)
+		return
+	}
 
 	handler := initPhysicalScreenHandler()
 	editor := createEditor(handler)
+	editor.initData(data)
 
 	blinkr := initRealBlinker(editor)
 	editor.setBlinker(blinkr)
