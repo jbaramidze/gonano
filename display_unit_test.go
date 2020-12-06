40b25b514667fdc142078b5fea7b1b0f6007d546
Zhani Baramidze <zhani.baramidze@bolt.eu>
2020-12-06 17:11:11 +0200

    Finalized arrows tests

--- display_unit_test.go
+++ display_unit_test.go
@@ -68,18 +68,102 @@ func TestArrow(t *testing.T) {
 	sendKey(ctx, tcell.KeyDown)
 	expectParams(ctx, 0, 1, 2)
 
-	// Move to next line: visible beginning, non-visible end, fits screen
+	// Move to next line: visible beginning, fits screen, visible part
 	setupScenario(ctx, [][]rune{{'a'}, {'a'}, {'a', 'b', 'c', 'd'}}, 0, 1, 1)
 	sendKey(ctx, tcell.KeyDown)
 	expectParams(ctx, 1, 2, 1)
 
-	// Move to next line: visible beginning, non-visible end, does not fit screen V1
+	// Move to next line: visible beginning, fits screen, visible part, longer
 	setupScenario(ctx, [][]rune{{'a'}, {'a'}, {'a', 'b', 'c', 'd', 'e', 'f', 'g'}}, 0, 1, 1)
 	sendKey(ctx, tcell.KeyDown)
 	expectParams(ctx, 2, 2, 1)
 
-	// Move to next line: visible beginning, non-visible end, does not fit screen V2
+	// Move to next line: visible beginning, fits screen, invisible part
+	setupScenario(ctx, [][]rune{{'a'}, {'a', 'b', 'c', 'd'}, {'a', 'b', 'c', 'd'}}, 0, 1, 4)
+	sendKey(ctx, tcell.KeyDown)
+	expectParams(ctx, 2, 2, 4)
+
+	// Move to next line: visible beginning, does not fit screen, visible part
 	setupScenario(ctx, [][]rune{{'a'}, {'a'}, {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'i', 'j', 'k', 'l', 'm', 'n'}}, 0, 1, 1)
 	sendKey(ctx, tcell.KeyDown)
 	expectParams(ctx, 2, 2, 1)
+
+	// Move to next line: visible beginning, does not fit screen, invisible part
+	setupScenario(ctx, [][]rune{{'a', 'b', 'c', 'd'}, {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'i', 'j', 'k', 'l', 'm', 'n'}}, 0, 0, 4)
+	sendKey(ctx, tcell.KeyDown)
+	expectParams(ctx, 2, 1, 4)
+
+	// Move to next line from line that's end is not visible, newline - 1 lines
+	setupScenario(ctx, [][]rune{{'a'}, {'a'}, {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'i', 'j', 'k', 'l', 'm', 'n'}, {'a'}}, 2, 2, 1)
+	sendKey(ctx, tcell.KeyDown)
+	expectParams(ctx, 5, 3, 1)
+
+	// Move to next line from line that's end is not visible, newline - 2 lines, jump to beginning
+	setupScenario(ctx, [][]rune{{'a'}, {'a'}, {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'i', 'j', 'k', 'l', 'm', 'n'}, {'a', 'b', 'c', 'd'}}, 2, 2, 4)
+	sendKey(ctx, tcell.KeyDown)
+	expectParams(ctx, 6, 3, 4)
+
+	// Move to next line: non-visible beginning, non-visible end, fits screen v1
+	setupScenario(ctx, [][]rune{{'a'}, {'a'}, {'a'}, {'a', 'b'}}, 0, 2, 0)
+	sendKey(ctx, tcell.KeyDown)
+	expectParams(ctx, 1, 3, 0)
+
+	// Move to next line: non-visible beginning, non-visible end, fits screen v2
+	setupScenario(ctx, [][]rune{{'a'}, {'a'}, {'a'}, {'a', 'b', 'c', 'd'}}, 0, 2, 0)
+	sendKey(ctx, tcell.KeyDown)
+	expectParams(ctx, 2, 3, 0)
+
+	// Move to next line: non-visible beginning, non-visible end, does not fit screen
+	setupScenario(ctx, [][]rune{{'a'}, {'a'}, {'a'}, {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'i', 'j', 'k', 'l', 'm', 'n'}}, 0, 2, 0)
+	sendKey(ctx, tcell.KeyDown)
+	expectParams(ctx, 3, 3, 0)
+
+	/*
+		Up arrow test
+	*/
+
+	// Cannot go more up
+	setupScenario(ctx, [][]rune{{'a', 'b'}, {'b', 'c', 'd', 'e'}}, 0, 0, 2)
+	sendKey(ctx, tcell.KeyUp)
+	expectParams(ctx, 0, 0, 2)
+
+	// Move to previous longer line
+	setupScenario(ctx, [][]rune{{'a', 'b', 'c'}, {'b', 'c'}}, 0, 1, 1)
+	sendKey(ctx, tcell.KeyUp)
+	expectParams(ctx, 0, 0, 1)
+
+	// Move to previous shorter line
+	setupScenario(ctx, [][]rune{{'a', 'b'}, {'b', 'c', 'd'}}, 0, 1, 3)
+	sendKey(ctx, tcell.KeyUp)
+	expectParams(ctx, 0, 0, 2)
+
+	// Move to previous line: visible end, non-visible beginning, fits screen v1
+	setupScenario(ctx, [][]rune{{'a'}, {'a'}, {'a', 'b'}, {'a', 'b', 'c', 'd'}}, 3, 3, 1)
+	sendKey(ctx, tcell.KeyUp)
+	expectParams(ctx, 2, 2, 1)
+
+	// Move to previous line: visible end, non-visible beginning, fits screen v2
+	setupScenario(ctx, [][]rune{{'a'}, {'a', 'b', 'c', 'd'}, {'a', 'b', 'c', 'd'}}, 3, 2, 1)
+	sendKey(ctx, tcell.KeyUp)
+	expectParams(ctx, 1, 1, 1)
+
+	// Move to previous line: visible end, non-visible beginning, fits screen v3
+	setupScenario(ctx, [][]rune{{'a'}, {'a', 'b', 'c', 'd'}, {'a', 'b', 'c', 'd'}}, 3, 2, 4)
+	sendKey(ctx, tcell.KeyUp)
+	expectParams(ctx, 1, 1, 4)
+
+	// Move to previous line: visible end, non-visible beginning, doesn't fit screen v1
+	setupScenario(ctx, [][]rune{{'a'}, {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'i', 'j', 'k', 'l', 'm', 'n'}, {'a'}}, 6, 2, 0)
+	sendKey(ctx, tcell.KeyUp)
+	expectParams(ctx, 1, 1, 0)
+
+	// Move to previous line: visible end, non-visible beginning, doesn't fit screen v2
+	setupScenario(ctx, [][]rune{{'a'}, {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'i', 'j', 'k', 'l', 'm', 'n'}, {'a', 'b', 'c', 'd'}}, 6, 2, 4)
+	sendKey(ctx, tcell.KeyUp)
+	expectParams(ctx, 1, 1, 4)
+
+	// Move to previous line: visible end, non-visible beginning, doesn't fit screen v2
+	setupScenario(ctx, [][]rune{{'a'}, {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'i', 'j', 'k', 'l', 'm', 'n'}, {'a', 'b', 'c', 'd'}}, 7, 2, 4)
+	sendKey(ctx, tcell.KeyUp)
+	expectParams(ctx, 1, 1, 4)
 }
